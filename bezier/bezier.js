ea0574f5f8d03be1e6187299c63d19ea2a9e2e5c
Vincent Matheo Köckerling <vincent-matheo.koeckerling@stud.hs-hannover.de>
2023-06-28 23:33:17 +0200

    Graph visual

--- bezier/bezier.js
+++ bezier/bezier.js
@@ -6,50 +6,93 @@ document.getElementById('t-range').addEventListener('input', (e) => t = e.curren
 const canvasElement = document.getElementById('canvas')
 const two = new Two({ fitted: true }).appendTo(canvasElement)
 
-const point1 = createControlPoint(two.width / 8 * 1, two.height / 8 * 5)
-const point2 = createControlPoint(two.width / 8 * 2, two.height / 4)
-const point3 = createControlPoint(two.width / 8 * 6, two.height / 4)
-const point4 = createControlPoint(two.width / 8 * 7, two.height / 8 * 5)
+// -- Setup
 
-const line12 = createLine(point1, point2)
-const line23 = createLine(point2, point3)
-const line34 = createLine(point3, point4)
+const resultPath = two.makePath()
+resultPath.noFill()
+resultPath.closed = false
+resultPath.stroke = 'rgb(0, 0, 255)';
 
-const point12 = createInterpolPoint(point1, point2)
-const point23 = createInterpolPoint(point2, point3)
-const point34 = createInterpolPoint(point3, point4)
+// Lines
 
-const line12_23 = createLine(point12.point, point23.point)
-const line23_34 = createLine(point23.point, point34.point)
+const line1_2 = makeLine()
+const line2_3 = makeLine()
+const line3_4 = makeLine()
 
-const point12_23 = createInterpolPoint(point12.point, point23.point)
-const point23_34 = createInterpolPoint(point23.point, point34.point)
+const line1_2__2_3 = makeLine()
+const line2_3__3_4 = makeLine()
 
-const line12_23__23_34 = createLine(point12_23.point, point23_34.point)
-const point12_23__23_34 = createInterpolPoint(point12_23.point, point23_34.point)
+const line1_2_3_4 = makeLine()
 
-function render() {
-	line12.update()
-	line23.update()
-	line34.update()
+// Circles
 
-	point12.update(t)
-	point23.update(t)
-	point34.update(t)
+const point1 = makeDraggableCircle(two.width / 8 * 1, two.height / 8 * 5)
+const point2 = makeDraggableCircle(two.width / 8 * 2, two.height / 4)
+const point3 = makeDraggableCircle(two.width / 8 * 6, two.height / 4)
+const point4 = makeDraggableCircle(two.width / 8 * 7, two.height / 8 * 5)
 
-	line12_23.update()
-	line23_34.update()
+const point1_2 = two.makeCircle(0, 0, 10)
+const point2_3 = two.makeCircle(0, 0, 10)
+const point3_4 = two.makeCircle(0, 0, 10)
 
-	point12_23.update(t)
-	point23_34.update(t)
+const point1_2__2_3 = two.makeCircle(0, 0, 10)
+const point2_3__3_4 = two.makeCircle(0, 0, 10)
 
-	line12_23__23_34.update()
+const resultPoint = two.makeCircle(0, 0, 10)
 
-	point12_23__23_34.update(t)
+// Update
+
+function update() {
+	const position1 = point1.position
+	const position2 = point2.position
+	const position3 = point3.position
+	const position4 = point4.position
+
+	// Points
+
+	point1_2.position = lerp(position1, position2, t)
+	point2_3.position = lerp(position2, position3, t)
+	point3_4.position = lerp(position3, position4, t)
+
+	point1_2__2_3.position = quadraticLerp(position1, position2, position3, t)
+	point2_3__3_4.position = quadraticLerp(position2, position3, position4, t)
+
+	resultPoint.position = cubicLerp(position1, position2, position3, position4, t)
+
+	// Lines
+
+	line1_2.position1 = position1
+	line1_2.position2 = position2
+
+	line2_3.position1 = position2
+	line2_3.position2 = position3
+
+	line3_4.position1 = position3
+	line3_4.position2 = position4
+
+	line1_2__2_3.position1 = lerp(position1, position2, t)
+	line1_2__2_3.position2 = lerp(position2, position3, t)
+
+	line2_3__3_4.position1 = lerp(position2, position3, t)
+	line2_3__3_4.position2 = lerp(position3, position4, t)
+
+	line1_2_3_4.position1 = quadraticLerp(position1, position2, position3, t)
+	line1_2_3_4.position2 = quadraticLerp(position2, position3, position4, t)
+
+	// Result path
+
+	resultPath.vertices.length = 0
+
+	for (let t = 0; t <= 1; t += 0.01) {
+		const position = cubicLerp(position1, position2, position3, position4, t)
+		resultPath.vertices.push(vectorToAnchor(position))
+	}
 }
 
-two.bind('update', render)
+two.bind('update', update)
 two.play()
+
+// Draggable
 
 let currentClickedPoint = null
 
@@ -62,7 +105,7 @@ canvasElement.addEventListener('mousemove', (e) => {
 	currentClickedPoint.position.y += e.movementY * 1.25
 })
 
-function createControlPoint(x, y) {
+function makeDraggableCircle(x, y) {
 	const point = two.makeCircle(x, y, 15)
 
 	two.update()
@@ -71,36 +114,50 @@ function createControlPoint(x, y) {
 	return point
 }
 
-function createLine(point1, point2) {
-	const line = two.makeLine(point1.position.x, point1.position.y, point2.position.x, point2.position.y)
+// Util
 
-	function update() {
-		line.vertices[0].x = point1.position.x
-		line.vertices[0].y = point1.position.y
-		line.vertices[1].x = point2.position.x
-		line.vertices[1].y = point2.position.y
+function makeLine() {
+	const line = two.makeLine(0, 0, 0, 0)
+
+	function setPosition(index, vector) {
+		line.vertices[index].x = vectorToAnchor(vector).x
+		line.vertices[index].y = vectorToAnchor(vector).y
 	}
 
 	return {
 		line,
-		update
+		set position1(vector) {
+			setPosition(0, vector)
+		},
+		set position2(vector) {
+			setPosition(1, vector)
+		}
 	}
 }
 
-function createInterpolPoint(point1, point2) {
-	const point = two.makeCircle(0, 0, 15)
+function vectorToAnchor(vector) {
+	return new Two.Anchor(vector.x, vector.y)
+}
 
-	function update(t) {
-		point.position.x = lerp(point1.position.x, point2.position.x, t)
-		point.position.y = lerp(point1.position.y, point2.position.y, t)
-	}
+// DeCasteljau
 
-	return {
-		point,
-		update
-	}
+function cubicLerp(a, b, c, d, t) {
+	const ab_bc = quadraticLerp(a, b, c, t)
+	const bc_cd = quadraticLerp(b, c, d, t)
+
+	return lerp(ab_bc, bc_cd, t)
+}
+
+function quadraticLerp(a, b, c, t) {
+	const ab = lerp(a, b, t)
+	const bc = lerp(b, c, t)
+
+	return lerp(ab, bc, t)
 }
 
 function lerp(a, b, t) {
-	return (1-t) * a + t * b
+	return new Two.Vector(
+		(1 - t) * a.x + t * b.x,
+		(1 - t) * a.y + t * b.y
+	)
 }
