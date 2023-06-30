9c974cbebe201932eafee039ee6439e506e59d02
Anne Greve <anne_greve@web.de>
2023-06-30 13:02:14 +0200

    Hermite angefangen

--- hermite/hermite.js
+++ hermite/hermite.js
@@ -4,7 +4,7 @@ const canvasElement = document.getElementById('side_canvas')
 const two = new Two({ fitted: true }).appendTo(canvasElement)
 
 two.makeArrow(0, two.height, 0, 0)
-two.makeArrow(0, two.height*0.8, two.width, two.height*0.8)
+two.makeArrow(0, two.height * 0.8, two.width, two.height * 0.8)
 
 //Zeichne H0(t) = 2𝑡3 − 3𝑡2 + 1
 const path = two.makePath();
@@ -13,7 +13,7 @@ path.stroke = '#D08240';
 
 for (let x = 0; x <= 100; x++) {
     let y = 2 * Math.pow(x / 100, 3) - 3 * Math.pow(x / 100, 2) + 1;
-    let pos = new Two.Vector((x / 100) * two.width, 0.8*two.height -y * 0.8*two.height);
+    let pos = new Two.Vector((x / 100) * two.width, 0.8 * two.height - y * 0.8 * two.height);
     path.vertices.push(pos);
 }
 
@@ -24,7 +24,7 @@ path2.stroke = '#CA40D0';
 
 for (let x = 0; x <= 100; x++) {
     let y = Math.pow(x / 100, 3) - 2 * Math.pow(x / 100, 2) + x / 100;
-    let pos = new Two.Vector((x / 100) * two.width, 0.8*two.height - y * 0.8*two.height);
+    let pos = new Two.Vector((x / 100) * two.width, 0.8 * two.height - y * 0.8 * two.height);
     path2.vertices.push(pos);
 }
 
@@ -35,7 +35,7 @@ path3.stroke = '#408ed0';
 
 for (let x = 0; x <= 100; x++) {
     let y = -2 * Math.pow(x / 100, 3) + 3 * Math.pow(x / 100, 2);
-    let pos = new Two.Vector((x / 100) * two.width, 0.8*two.height - y * 0.8*two.height);
+    let pos = new Two.Vector((x / 100) * two.width, 0.8 * two.height - y * 0.8 * two.height);
     path3.vertices.push(pos);
 }
 
@@ -46,9 +46,35 @@ path4.stroke = '#46D040';
 
 for (let x = 0; x <= 100; x++) {
     let y = Math.pow(x / 100, 3) - Math.pow(x / 100, 2);
-    let pos = new Two.Vector((x / 100) * two.width, 0.8*two.height - y * 0.8*two.height);
+    let pos = new Two.Vector((x / 100) * two.width, 0.8 * two.height - y * 0.8 * two.height);
     path4.vertices.push(pos);
 }
 
+
+//////////////////////////////
+const mainCanvas = document.getElementById('hermite_canvas')
+const two2 = new Two({ fitted: true }).appendTo(mainCanvas)
+
+const curve = two2.makePath();
+curve.noFill().closed = false;
+curve.stroke = '#D08240';
+
+let m1 = new Two.Vector(403, 136)
+let m0 = new Two.Vector(159, 264)
+let p0 = new Two.Vector(102, 209)
+let p1 = new Two.Vector(377, 184)
+curve.vertices.push(p0, p1);
+var p0c = two2.makeCircle(p0.x, p0.y, 5);
+p0c.stroke ='#b5b5b5';
+var p1c = two2.makeCircle(p1.x, p1.y, 5);
+p1c.stroke ='#b5b5b5';
+
+two2.makeArrow(p0.x, p0.y, m0.x, m0.y);
+two2.makeArrow(p1.x, p1.y, m1.x, m1.y)
+
+
+
+
 // Don’t forget to tell two to draw everything to the screen
 two.update();
+two2.update();
