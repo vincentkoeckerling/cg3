5393a5a70997a300233dadb6ab9353c77f761001
Anne473 <80322420+Anne473@users.noreply.github.com>
2023-07-06 18:02:42 +0200

    Punkte hinzugefügt

--- main.js
+++ main.js
@@ -8,34 +8,26 @@ controlCenter.onPlayButtonClicked();
 controlCenter.setT(0);
 
 const mainCanvas = document.getElementById('curve_canvas')
+const mainCanvas2 = document.getElementById('curve_canvas2')
 const two = new Two({ fitted: true }).appendTo(mainCanvas)
+const two2 = new Two({ fitted: true }).appendTo(mainCanvas2)
 const curve = two.makePath();
 
 curve.noFill().closed = false;
-curve.stroke = '#181818';
-curve.linewidth = 3
-
-
+curve.stroke = '#808080';
+curve.linewidth = 3.6
 
 let h = two.height / 100
 let w = two.width / 100
 
 let a0 = [10 * w, 35 * h]
 let a1 = [0 * w, 80 * h]
-let b0 = [5 * w, 40 * h]
-let b1 = [5 * w, 10 * h]
 let c0 = [10 * w, 12 * h]
 let c1 = [24 * w, 12 * h]
-let d0 = [18 * w, 45 * h]
-let d1 = [18 * w, 85 * h]
 let e0 = [17 * w, 75 * h]
 let e1 = [12 * w, 95 * h]
 let f0 = [15 * w, 60 * h]
 let f1 = [25 * w, 40 * h]
-let g0 = [25 * w, 70 * h]
-let g1 = [30 * w, 70 * h]
-let h0 = [25 * w, 80 * h]
-let h1 = [25 * w, 60 * h]
 let i0 = [30 * w, 55 * h]
 let i1 = [45 * w, 75 * h]
 let j0 = [43 * w, 70 * h]
@@ -54,57 +46,88 @@ let p0 = [87 * w, 85 * h]
 let p1 = [74 * w, 92 * h]
 
 
-var pointArray = [a0, a1, c0, c1, e0, e1, f0, f1, i0, i1, j0, j1, k0, k1, l0, l1, m0, m1, n0, n1, o0, o1, p0, p1,]
+var pointArray = [a0, a1, c0, c1, e0, e1, f0, f1, i0, i1, j0, j1, k0, k1, l0, l1, m0, m1, n0, n1, o0, o1, p0, p1]
 
-/*for (var i = 0; i < pointArray.length; i += 2) {
-    var pointy = two.makeCircle(pointArray[i][0], pointArray[i][1], 2)
-    pointy.stroke = '#808080'
-    pointy.fill = '#808080'
-    pointy.opacity = 0.4
-}*/
+controlCenter.tMax = 28
 
-for (var i = 1; i < pointArray.length; i += 2) {
-    // two.makeCircle(pointArray[i][0], pointArray[i][1], 2)
-}
-
-controlCenter.tMax = pointArray.length / 2 - 1 + 12
 controlCenter.tRate = 0.0247
 
+
+var i = 0
 function update() {
+
     if (!controlCenter.shouldUpdate()) return
-    tPoint(pointArray)
 
+    if (controlCenter.t < 11.5) {
+        drawPoints()
+    }
+    
+    if (controlCenter.t >= 12 && controlCenter.t <=23) {
+        
+        tPoint(pointArray)
+    }
 
+    if(controlCenter.t >24 && controlCenter.t < 24.1){
+        two2.clear()
+    }
+
+    if(controlCenter.t >24){
+        fadeOut()
+    }
 }
 
 two.bind('update', update)
 two.play()
+two2.bind('update', update)
+two2.play()
 
 mainCanvas.addEventListener('load', (e) => {
     controlCenter.animate();
 })
 
-
-
+function drawPoints() {
+    var t = controlCenter.t;
+    controlCenter.tRate = 0.047
+    let x = t % 1
+    var l = floor(t)*2 
+    
+    if (x <= 0.047) {
+        //ersten und letzten Punkt leicht verschieben, damit sie von der Linie verdeckt werden
+        if (l<=1){ 
+            var point = two2.makeCircle(9.5 * w, 37 * h, 1.4) 
+            point.stroke = '#808080'
+            point.fill = '#808080'
+            return
+        }
+        if (l>=21){ 
+            var point = two2.makeCircle(87.5 * w, 84.45 * h, 1.4) 
+            point.stroke = '#808080'
+            point.fill = '#808080'
+            return
+        }
+        var point = two2.makeCircle(pointArray[l][0], pointArray[l][1], 1.4)
+        point.stroke = '#808080'
+        point.fill = '#808080'
+    }
+}
 
 function tPoint(array) {
-    let t = controlCenter.t;
+    
+    var offset = 12
+    var t = controlCenter.t - offset;
     let x = t % 1
     var c = floor(t) * 2
+    controlCenter.tRate = 0.0247
 
-
-    if (t >= (array.length / 2) - 1.02 + 4) {
-        curve.opacity -=0.004
-        if(curve.opacity <= 0.01){
-            curve.vertices = 0
-        }
+    if (t >= array.length - 1.0 + 4 - offset) {
+        fadeOut()
     }
 
-    if (t<=0.1){
-        curve.opacity =0.8
+    if (t <= 0.1) {
+        curve.opacity = 1
     }
 
-    if (t >= (array.length / 2) - 1.02) {
+    if (t >= (array.length / 2) - 1.0) {
         return
     }
 
@@ -126,10 +149,16 @@ function tPoint(array) {
         + (Math.pow(x, 3) - Math.pow(x, 2)) * mp1[1];
 
     curve.vertices.push(new Two.Vector(xx, y))
-
-
-
+   
 }
 
-// Don’t forget to tell two to draw everything to the screen
+function fadeOut(){
+    curve.opacity -= 0.004
+        if (curve.opacity <= 0.01) {
+            curve.vertices = 0
+        }
+}
+
+// draw everything to the screen
 two.update();
+two2.update();
